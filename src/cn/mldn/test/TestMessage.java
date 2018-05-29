8fe82730ec3114584d463c37883748dfaa3650e0
wang <saukes@163.com>
2018-05-29 21:02:20 +0800

    修改了TestMessage类中的备注

--- src/cn/mldn/test/TestMessage.java
+++ src/cn/mldn/test/TestMessage.java
@@ -5,6 +5,7 @@ import cn.mldn.service.impl.MessageImpl;
 
 public class TestMessage {
 	public static void main(String[] args) {
+		//保存所有类中的分支中的信息
 		IMessage msg = new MessageImpl();
 		System.out.println(msg.echo("www.mldn.cn"));
 	}
